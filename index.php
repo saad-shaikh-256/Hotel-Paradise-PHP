859d65fe9361f39feeab8f409df38ec6dea3bac9
Saad Shaikh <shaikhsaad256@gmail.com>
2024-10-27 18:28:15 +0530

    Welcome Page Animation

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+header("Refresh: 3; url=User Side/PHP/landing-page.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Hotel Paradise - by shaikhsaad256@gmail.com</title>
+
+   <link rel="shortcut icon" href="Assets/Common/Vectors/Logo.svg" type="image/x-icon">
+   <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+   <h1 class="loading">Welcome</h1>
+
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/gsap.min.js" integrity="sha512-7eHRwcbYkK4d9g/6tD/mhkf++eoTHwpNM9woBxtPUBWm67zeAfFC+HrdoE2GanKeocly/VxeLvIqwvCdk7qScg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+   <script src="script.js"></script>
+</body>
+
+</html>
---
+++ script.js
@@ -0,0 +1,32 @@
+function breakTheText() {
+   var h1 = document.querySelector("h1");
+   var h1Text = h1.textContent;
+
+   var splittedText = h1Text.split("").map(char => {
+      // If the character is a space, return a span with a non-breaking space
+      return char === " " ? "&nbsp;" : char; // or return `<span>&nbsp;</span>` if you prefer
+   });
+
+   var clutter = "";
+
+   splittedText.forEach(function (elem) {
+      clutter += `<span>${elem}</span>`;
+   });
+
+   h1.innerHTML = clutter;
+}
+
+breakTheText();
+
+gsap.from("h1 span", {
+   y: 50,
+   duration: 0.6,
+   delay: 0.5,
+   stagger: 0.15,
+});
+gsap.to("h1 span", {
+   y: 0,
+   duration: 0.6,
+   delay: 0.5,
+   stagger: 0.15,
+});
