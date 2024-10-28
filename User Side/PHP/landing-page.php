ee59c130bfe8f8ff1f1e7b4e2529ca23ecbf0fdf
Saad Shaikh <shaikhsaad256@gmail.com>
2024-10-28 16:17:14 +0530

    Home page with 100% Responsiveness

--- User Side/PHP/landing-page.php
+++ User Side/PHP/landing-page.php
@@ -6,24 +6,25 @@
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>Hotel Paradise - by shaikhsaad256@gmail.com</title>
 
-   <meta http-equiv="refresh" content="1">
+   <!-- <meta http-equiv="refresh" content="1"> -->
 
    <link rel="shortcut icon" href="../../Assets/Common/Vectors/Logo.svg" type="image/x-icon">
    <link rel="stylesheet" href="../CSS/landing-page.css">
+
 </head>
 
 <body>
 
-   <section id="nav">
-      <nav>
+   <section id="header">
+      <section id="nav">
+         <nav>
 
-         <div id="navLogo">
-            <img src="../../Assets/Common/Vectors/Logo.svg" alt="">
-         </div>
-         
+            <div id="navLogo">
+               <img src="../../Assets/Common/Vectors/Logo.svg" alt="">
+            </div>
 
             <div id="navPages">
-               <a href="#">Home</a>
+               <a href="#Home">Home</a>
                <a href="#">Rooms</a>
                <a href="#">Events</a>
                <a href="#">About</a>
@@ -36,16 +37,83 @@
                <a href="#">Logout</a>
             </div>
 
-         <div id="navBurger">
-            <img src="../../Assets/User Side/Vectors/navBurger.svg" alt="">
-         </div>
+            <div id="navBurger">
+               <img src="../../Assets/User Side/Vectors/navBurger.svg" alt="">
+            </div>
 
-      </nav>
+         </nav>
+      </section>
+
+      <section id="Home">
+         <div id="homeBox"></div>
+         <div id="homeContent">
+            <div id="homeTagline">Welcome to Paradise</div>
+            <div id="homeLorem">Hotel & Resort</div>
+         </div>
+      </section>
    </section>
 
+
+   <section id="Rooms">
+      <div id="roomTitle">Our Rooms
+         <hr>
+      </div>
+
+      <div id="roomGrid">
+
+         <div class="roomCard">
+            <img src="" alt="">
+            <div class="cardContent">
+               <div class="cardTitle">Standard Room</div>
+               <div class="cardPrice">$350.00 / per night</div>
+            </div>
+         </div>
+
+         <div class="roomCard">
+            <img src="" alt="">
+            <div class="cardContent">
+               <div class="cardTitle">Family Room</div>
+               <div class="cardPrice">$400.00 / per night</div>
+            </div>
+         </div>
+
+         <div class="roomCard">
+            <img src="" alt="">
+            <div class="cardContent">
+               <div class="cardTitle">Single Room</div>
+               <div class="cardPrice">$255.00 / per night</div>
+            </div>
+         </div>
+
+         <div class="roomCard">
+            <img src="" alt="">
+            <div class="cardContent">
+               <div class="cardTitle">Deluxe Room</div>
+               <div class="cardPrice">$150.00 / per night</div>
+            </div>
+         </div>
+
+         <div class="roomCard">
+            <img src="" alt="">
+            <div class="cardContent">
+               <div class="cardTitle">Luxury Room</div>
+               <div class="cardPrice">$200.00 / per night</div>
+            </div>
+         </div>
+
+         <div class="roomCard">
+            <img src="" alt="">
+            <div class="cardContent">
+               <div class="cardTitle">Single Room</div>
+               <div class="cardPrice">$155.00 / per night</div>
+            </div>
+         </div>
+
+      </div>
+
+   </section>
 
    <script src="../JS/landing-page.js"></script>
 </body>
 
 </html>
-<!-- Upto Here -->
